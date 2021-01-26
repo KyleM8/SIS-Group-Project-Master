c9f95ed88e0fb946fba3dafb77070103a61aad45
Kyle Markel <kylejm220@gmail.com>
2021-01-26 14:54:16 -0700

    addition and deletion function added
    Addition and deleteion function added.
    
    Added an InputHelper class for methods related to checking input and error management.
    
    Automatically modified Eclipse files were also added.

---
+++ src/InputHelper.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class InputHelper {
+	
+	public static boolean checkInput(String str) { //given a string, asks the user if it is the correct input, and returns a boolean based on the user's response
+		System.out.println("Is this the correct input? Type y for yes, or n for no.");
+		System.out.println(str);
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		if (input.toLowerCase().equals("y")) {
+			return true;
+		}
+		else if (input.toLowerCase().equals("n")) {
+			return false;
+		}
+		else {
+			checkInput(str);
+		}
+		return false;
+	}
+	
+	public static int getIndexOfStudent(String name) { //given the first and last name of a student separated by a space, search the array of students to find a student with a matching name. Returns -1 if the student does not exist.
+		for (int i = 0; i < Main.students.size(); i++) {
+			if (Main.students.get(i).getName().equals(name)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public static boolean isNumber(String str) { //given a string, this method will return true if the string is a number
+		try {
+			float num = Float.parseFloat(str);
+		}
+		catch (NumberFormatException e) {
+			System.out.println("Input format is incorrect, returning to top menu.");
+			Main.selectOption();
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean isChoice(String... params) { //Given an unlimited number of strings, returns true if the first parameter matches any of the following parameters. Helpful to ensure the input actually matches one of the given options.
+		int counter = 0;
+		String choice = "";
+		ArrayList<String> list = new ArrayList<String>();
+		for (String str : params) {
+			if (counter == 0) {
+				choice = str;
+			}
+			else {
+				list.add(str);
+			}
+		}
+		for (String str : list) {
+			if (choice.equals(str)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
--- src/StudentAdditionOrDeletion.java
+++ src/StudentAdditionOrDeletion.java
@@ -1,8 +1,74 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
-public class StudentAdditionOrDeletion 
-{
-	public static void addOrDeleteStudent()
-	{
-		
+public class StudentAdditionOrDeletion {
+	
+	public static void addOrDeleteStudent() { //addition/deletion main menu
+		System.out.println("");
+		System.out.println("Add or delete a student.");
+		System.out.println("1. Add student\n2. Delete student\n3. Return to main menu");
+		Scanner scanner = new Scanner(System.in);
+		String choiceStr = scanner.nextLine();
+		if (InputHelper.isChoice(choiceStr, "1", "2", "3") && InputHelper.isNumber(choiceStr)) {
+			int choice = scanner.nextInt();
+			if (choice == 1) { addStudent(); }
+			else if (choice == 2) { deleteStudent(); }
+			else if (choice == 3) { Main.selectOption(); }
+			else { addOrDeleteStudent(); }
+		}
+		else { addOrDeleteStudent(); }
 	}
+	
+	public static void addStudent() { //adds a student using user input
+		System.out.println("\nYou are now adding a new student.\nWhat is the student's name?");
+		Scanner nameScanner = new Scanner(System.in);
+		String name = nameScanner.nextLine();
+		if (InputHelper.checkInput(name)) {
+			System.out.println("The student's name is: " + name);
+			System.out.println("Now, input the student's subjects and grades in the format: Subject, Grade, Subject, Grade, Subject, Grade.");
+			System.out.println("Separate each element using a comma followed by a space. Do not use any punctuation other than commas and spaces.");
+			Scanner gradesScanner = new Scanner(System.in);
+			String gradesStr = gradesScanner.nextLine();
+			if (InputHelper.checkInput(gradesStr)) {
+				String [] gradesArr = gradesStr.split(", ");
+				ArrayList<String> tempSubjects = new ArrayList<String>();
+				Map<String,String> tempGrades = new HashMap<String,String>();
+				try {
+					for (int i = 0; i < gradesArr.length; i+=2) {
+						tempSubjects.add(gradesArr[i]);
+						tempGrades.put(gradesArr[i], gradesArr[i+1]);
+					}
+					Main.students.add(new Student(name, tempGrades, tempSubjects));
+				}
+				catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println("Sorry, it appears that you made a mistake in your input.\nReturning to top menu.");
+					Main.selectOption();
+				}
+			}
+			else { addOrDeleteStudent(); }
+		}
+		else { addOrDeleteStudent(); }
+	}
+	
+	public static void deleteStudent() { //removes a student based on his/her name
+		System.out.println("\nYou are now deleting a student.\nWhat is the student's name?\nType the student's first and last name below, separated by a space.");
+		Scanner nameScanner = new Scanner(System.in);
+		String name = nameScanner.nextLine();
+		int ind = InputHelper.getIndexOfStudent(name);
+		if (ind == -1) {
+			System.out.println("This student does not exist. Returning to the main menu.");
+			Main.selectOption();
+		}
+		else {
+			if (InputHelper.checkInput(name)) {
+				System.out.println("Deleting student...");
+				Main.students.remove(ind);
+				System.out.print("deleted.\n");
+			}
+			else { addOrDeleteStudent(); }
+		}
+	}
+	
 }
