1842252859a867717308bcd34b7d3718cd61d950
Liz2107 <escutting@gmail.com>
2021-01-27 12:37:36 -0700

    liz test commit two from newly forked repo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-//liz test comment
+//liz test comment two
 public class Main {
 	public static ArrayList<Student> students = new ArrayList<Student>();
 
