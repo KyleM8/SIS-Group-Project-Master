b5ef9f1a08d0259d6d39533d3c7fa1542bc08758
Kyle Markel <kylejm220@gmail.com>
2021-02-01 09:46:14 -0700

    code moved to SortLastName class

--- src/SortLastName.java
+++ src/SortLastName.java
@@ -1,4 +1,10 @@
+import java.util.Collections;
 
 public class SortLastName {
-
+	public static void sortLastName() {
+		Collections.sort(students);
+		for(int i=0; i<students.size(); i++)
+		    System.out.println(students.get(i));
+	}
+	
 }
--- src/StudentSorting.java
+++ src/StudentSorting.java
@@ -1,6 +1,14 @@
 
+<<<<<<< Updated upstream
 public class StudentSorting {
 	public static void sortStudentsMain() {
 		System.out.println("Sort Students\n1. Sort by last name\n2. Sort by GPA\n3. Sort by period");
+=======
+public class StudentSorting 
+{
+	public static void sortStudents()
+	{
+		
+>>>>>>> Stashed changes
 	}
 }
