1e7359ec6e922933f371e37931e4db6a5206567e
Kyle Markel <kylejm220@gmail.com>
2021-02-04 09:21:26 -0700

    deleted group sorter method

--- src/LastNameComparator.java
+++ src/LastNameComparator.java
@@ -1,11 +1,7 @@
 import java.util.Comparator;
 
-//daily pull request
 public class LastNameComparator implements Comparator<Student> {
-<<<<<<< Updated upstream
-=======
 	public int compare(Student s1, Student s2) {
->>>>>>> Stashed changes
 		return (s1.getLastName().compareToIgnoreCase(s2.getLastName()));	
 	}
 }
