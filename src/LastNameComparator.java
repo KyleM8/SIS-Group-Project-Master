73f1b407224d8fdaa810d0a7f35cca14123b7c8e
KyleM8 <kylejm220@gmail.com>
2021-02-05 09:22:39 -0700

    fixed github error

--- src/LastNameComparator.java
+++ src/LastNameComparator.java
@@ -1,10 +1,7 @@
 import java.util.Comparator;
 
 public class LastNameComparator implements Comparator<Student> {
-<<<<<<< Updated upstream
-=======
 	public int compare(Student s1, Student s2) {
->>>>>>> Stashed changes
 		return (s1.getLastName().compareToIgnoreCase(s2.getLastName()));	
 	}
 }
